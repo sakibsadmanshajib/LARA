da1c07d801db6b6a96129aa4d1f80b752b2ab267
sakibsadmanshajib <sakibsadmanshajib@users.noreply.github.com>
2018-07-16 01:30:48 +0600

    Added support for removing
    Added support for removing elements from list using BRAND, MODEL, CPU, RAM, GPU

--- updateList.py
+++ updateList.py
@@ -16,15 +16,21 @@ def addLaptop(brand, model, cpu, ram, gpu, ssd, hdd, price, lists):
     lists.append(laptop)
 
 def importLaptop(file, lists):
+
     with open(file, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
+
         for row in spamreader:
+
             comp = row[0].split(',')
-            addLaptop(comp[0], comp[1], comp[2], comp[3], comp[4], comp[5], comp[6], comp[7], lists)
+            addLaptop(comp[0], comp[1], comp[2], comp[3], comp[4], comp[5], comp[6], float(comp[7]), lists)
     
-Laptops = []
+def removeLaptop(brand, model, cpu, ram, gpu, lists):
+    
+    for record in lists:
 
-importLaptop('Laptops.csv', Laptops)
-
-print(Laptops)
-#def removeLaptop(brand, model, cpu, ram, gpu, lists):
+        if record['brand'] == brand and record['model'] == model and record['cpu'] == cpu and record['ram'] == ram and record['gpu'] == gpu:
+            lists.remove(record)
+            
+        else:
+            print("Sorry, the item you are trying to delete is not in the database.")
