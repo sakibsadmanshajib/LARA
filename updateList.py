4f2fbcc3beb9a7f2400346b25d4ca660ef2e0e0d
sakibsadmanshajib <sakibsadmanshajib@users.noreply.github.com>
2018-07-20 06:16:53 +0600

    Added OOP support
    Contains functions to manipulate the Laptop class. Also in development

--- updateList.py
+++ updateList.py
@@ -1,21 +1,14 @@
+from Laptop import *
+
 import csv
 
-def addLaptop(brand, model, cpu, ram, gpu, ssd, hdd, price, lists):
+def addLaptop(brand, model, cpu, ram, gpu, ssd, hdd, price):
 
-    laptop = {}
+    return Laptop(brand, model, cpu, ram, gpu, ssd, hdd, price)
 
-    laptop['brand'] = brand
-    laptop['model'] = model
-    laptop['cpu'] = cpu
-    laptop['ram'] = ram
-    laptop['gpu'] = gpu
-    laptop['ssd'] = ssd
-    laptop['hdd'] = hdd
-    laptop['price'] = price
+def importLaptop(file):
 
-    lists.append(laptop)
-
-def importLaptop(file, lists):
+    LPList = []
 
     with open(file, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter = ' ', quotechar = '|')
@@ -23,24 +16,23 @@ def importLaptop(file, lists):
         for row in spamreader:
 
             comp = row[0].split(',')
-            addLaptop(comp[0], comp[1], comp[2], comp[3], comp[4], comp[5], comp[6], float(comp[7]), lists)
+            LPList.append(Laptop(comp[0], comp[1], comp[2], comp[3], comp[4], comp[5], comp[6], float(comp[7])))
+
+    return LPList
     
 def removeLaptop(brand, model, cpu, ram, gpu, lists):
     
-    for record in lists:
+    for i in range(0, len(lists)):
 
-        if record['brand'] == brand and record['model'] == model and record['cpu'] == cpu and record['ram'] == ram and record['gpu'] == gpu:
-            lists.remove(record)
+        if lists[i].brand == brand and lists[i].model == model and lists[i].cpu == cpu and lists[i].ram == ram and lists[i].gpu == gpu:
+            lists.pop(i)
             
         else:
             print("Sorry, the item you are trying to delete is not in the database.")
 
-def editLaptop(brand, model, cpu, ram, gpu, type, change, lists):
-
-    for record in lists:
-
-        if record['brand'] == brand and record['model'] == model and record['cpu'] == cpu and record['ram'] == ram and record['gpu'] == gpu:
-            record[type] = change
-            
-        else:
-            print("Sorry, the item you are trying to edit is not in the database.")
+#def editLaptop(brand, model, cpu, ram, gpu, type, change, lists):
+#    for record in lists:
+#        if record['brand'] == brand and record['model'] == model and record['cpu'] == cpu and record['ram'] == ram and record['gpu'] == gpu:
+#            record[type] = change
+#        else:
+#            print("Sorry, the item you are trying to edit is not in the database.")
